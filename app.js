86ae9f2048c7b058b1649338b1dc9ffb019c2fb6
zep1994 <snaxbags@protonmail.com>
2019-10-20 03:21:25 -0400

    init express-gen

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-// const http = require('http')
-// // const time = require('./time')
-// const url = require('url')
-// const fs = require('fs')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// // http.createServer(function (req, res) {
-// //     res.writeHead(200, { 'Content-type': 'text/html'})
-// //     res.write("hello world\n")
-// //     //res.write("The date is:" + time.myDateTIme())
-// //     //var q = url.parse(req.url, true) .query
-// //     //var txt = q.year + " " + q.month;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-// //     res.end()
-// // }).listen(8000)
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// http.createServer(function (req, res) {
-    
-// })
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-const express = require('express')
-const app = express()
-const port = 9000
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.get('/', (req, res) => res.send("Hello World"))
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
